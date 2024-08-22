74add60da26690c772229f080b636cbf4fc39327
getahunTiruneh <gechtiru@gmail.com>
2024-08-22 22:04:47 +0300

    update unittests

--- tests/test_EDA.py
+++ tests/test_EDA.py
@@ -1,23 +1,41 @@
 import unittest
 import numpy as np
 import pandas as pd
+import sys 
+sys.path.insert(0, '/Kiffya_10_acc/SolarStrategy-Insight/scripts/')
 from scripts.Moonlight_EDA import DataPreprocessor
+
 
 # test class for the DataPreprocessor class
 class Test_DataPreprocessor(unittest.TestCase):
     def setUp(self):
-        self.data = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
-        self.preprocessor = DataPreprocessor(self.data)
-    
-    def test_load_data(self):
-        # Define
-        expected_data = self.data
-        # Act
-        actual_data = self.preprocessor.data
-        # Assert
-        self.assertTrue(np.array_equal(actual_data, expected_data))
+        self.df_with_missing = pd.DataFrame({
+            'A': [1, 2, None, 4],
+            'B': [5, None, 7, 8],
+            'C': [9, 10, 11, None]
+        })
+        self.df_without_missing = pd.DataFrame({
+            'A': [1, 2, 3, 4],
+            'B': [5, 6, 7, 8],
+            'C': [9, 10, 11, 12]
+        })
+        self.preprocessor = DataPreprocessor(pd.DataFrame()) 
+    def test_check_missing_values_with_missing(self):
+        # Arrange
+        expected_output = pd.Series([1, 1, 1], index=['A', 'B', 'C'])
 
-    def test_check_missing_values(self):
-        self.assertFalse(self.preprocessor.check_missing_values().any())
+        # Act
+        result = self.preprocessor.check_missing_values(self.df_with_missing)
+
+        # Assert
+        pd.testing.assert_series_equal(result, expected_output)
+
+    def test_check_missing_values_without_missing(self):
+        # Arrange
+        expected_output = pd.Series([0, 0, 0], index=['A', 'B', 'C'])
+        # Act
+        result = self.preprocessor.check_missing_values(self.df_without_missing)
+        # Assert
+        pd.testing.assert_series_equal(result, expected_output)
 if __name__ == '__main__':
     unittest.main()
