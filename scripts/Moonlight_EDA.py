260c4cb99ef840fda1e8f9f44b8ac591f36c9d2e
getahunTiruneh <gechtiru@gmail.com>
2024-08-23 13:13:44 +0300

    add EDA for data preprocessing

--- scripts/Moonlight_EDA.py
+++ scripts/Moonlight_EDA.py
@@ -11,9 +11,30 @@ class DataPreprocessor:
     def load_data(self):
          # Load the CSV file and store it in the data attribute
         self.data = pd.read_csv(self.file_path)
-
-    def check_missing_values(self,df):
-        self.missing_data = df.isnull().sum()
+        
+    # Calculate and return summary statistics for numeric columns
+    def summary_statistics(self):
+        stats=self.data.describe().T
+        return stats
+    
+    #Perform data quality checks including missing values and negative values
+    def data_quality_check(self):
+        
+        quality_report = pd.DataFrame()
+        
+        # Check for missing values
+        missing_values = self.data.isna().sum()
+        
+        # Check for negative values in specific columns
+        negative_values = (self.data[['GHI', 'DNI', 'DHI', 'ModA', 'ModB', 'WS', 'WSgust']] < 0).sum()
+        
+        # Compile the report
+        quality_report['Missing Values'] = missing_values
+        quality_report['Negative Values'] = negative_values
+        
+        return quality_report
+    def check_missing_values(self):
+        self.missing_data = self.data.isnull().sum()
         return self.missing_data
         
     def correlation(self):
