12f64d6f6adc9eadd68b102b42b3837bf05e71e9
getahunTiruneh <gechtiru@gmail.com>
2024-08-24 20:14:15 +0300

    unit test for EDA

--- tests/test_EDA.py
+++ tests/test_EDA.py
@@ -9,32 +9,37 @@ from scripts.Moonlight_EDA import DataPreprocessor
 # test class for the DataPreprocessor class
 class Test_DataPreprocessor(unittest.TestCase):
     def setUp(self):
+        self.preprocessor = DataPreprocessor('../data/benin-malanville.csv')
         self.df_with_missing = pd.DataFrame({
-            'A': [1, 2, None, 4],
-            'B': [5, None, 7, 8],
-            'C': [9, 10, 11, None]
+            'A': [1, 2, None],
+            'B': [4, None, 6],
+            'C': [None, 8, 9]
         })
         self.df_without_missing = pd.DataFrame({
-            'A': [1, 2, 3, 4],
-            'B': [5, 6, 7, 8],
-            'C': [9, 10, 11, 12]
+            'A': [1, 2, 3],
+            'B': [4, 5, 6],
+            'C': [7, 8, 9]
         })
-        self.preprocessor = DataPreprocessor(pd.DataFrame()) 
+        # self.preprocessor = DataPreprocessor(pd.DataFrame()) 
     def test_check_missing_values_with_missing(self):
         # Arrange
+        self.preprocessor.data = self.df_with_missing  # Set the internal data
         expected_output = pd.Series([1, 1, 1], index=['A', 'B', 'C'])
 
         # Act
-        result = self.preprocessor.check_missing_values(self.df_with_missing)
+        result = self.preprocessor.check_missing_values()
 
         # Assert
         pd.testing.assert_series_equal(result, expected_output)
 
     def test_check_missing_values_without_missing(self):
         # Arrange
+        self.preprocessor.data = self.df_without_missing  # Set the internal data
         expected_output = pd.Series([0, 0, 0], index=['A', 'B', 'C'])
+
         # Act
-        result = self.preprocessor.check_missing_values(self.df_without_missing)
+        result = self.preprocessor.check_missing_values()
+
         # Assert
         pd.testing.assert_series_equal(result, expected_output)
 if __name__ == '__main__':
